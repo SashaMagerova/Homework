393fcc9ab86f732a7d4b39b65fc6f6804378e0f2
Sasha <a.magerova@list.ru>
2022-12-08 01:04:19 +0300

    task4

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,17 +1,19 @@
-﻿// Задача2
+﻿// Задача4
 
-int a, b;
+int a, b, c;
 
 Console.Write($"Введите первое число: ");
 int.TryParse(Console.ReadLine()!, out a);
 Console.Write($"Введите второе число: ");
 int.TryParse(Console.ReadLine()!, out b);
+Console.Write($"Введите третье число: ");
+int.TryParse(Console.ReadLine()!, out c);
 
-if (a > b)
-{
-  Console.Write($"{a}");  
-}
-else
-{
-   Console.Write($"{b}"); 
-}
+int max = a;
+if (b > max)
+    max = b; 
+if (c > max)
+    max = c; 
+    
+Console.Write($"{max}");  
+
