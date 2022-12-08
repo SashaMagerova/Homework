7b6173c02b394728fe3c7579ebd0f0e6aeea9a3d
Sasha <a.magerova@list.ru>
2022-12-08 23:57:43 +0300

    newchangeintask8

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,14 +1,2 @@
-﻿// Задача8
-
-int N;
-Console.Write($"Введите число: ");
-int.TryParse(Console.ReadLine()!, out N);
-
-int index = 2;
-
-while (index <= N)
-{
-  if (index % 2 == 0);
-      Console.Write($"{index} ");
-      index += 2;
-}      
+﻿// 
+     
