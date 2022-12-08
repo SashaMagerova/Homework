5c5a7ad73f7f7b56f8870599e015d8fe9ff65177
Sasha <a.magerova@list.ru>
2022-12-08 23:54:16 +0300

    task10

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача10
+
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int B;
+B = N / 10;
+
+int C;
+C = B % 10;
+Console.WriteLine($"{C}");
+
+
