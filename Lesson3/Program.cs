28b534b58eaea1eef06dff11d80ed5c9e55d5331
Sasha <a.magerova@list.ru>
2022-12-11 14:43:27 +0300

    task19

---
+++ Lesson3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача19
+
+int A;
+Console.Write($"Введите 5ти значное число: ");
+int.TryParse(Console.ReadLine()!, out A);
+
+
+string A1 = A.ToString();
+
+if ( A1[0] == A1[4] && A1[1] == A1[3])
+    Console.WriteLine($"Да");
+
+else 
+    Console.WriteLine($"Нет"); 
