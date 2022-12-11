b33908035a7de8ad4c5aab7c02eed2b25e4da8d7
Sasha <a.magerova@list.ru>
2022-12-11 15:26:10 +0300

    task23

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,19 +1,10 @@
-﻿// Задача21
+﻿// Задача23
 
-int xa, xb, ya, yb, za, zb;
-Console.Write($"Введите координату X точки А: ");
-int.TryParse(Console.ReadLine()!, out xa);
-Console.Write($"Введите координату Y точки А: ");
-int.TryParse(Console.ReadLine()!, out ya);
-Console.Write($"Введите координату Z точки А: ");
-int.TryParse(Console.ReadLine()!, out za);
-Console.Write($"Введите координату X точки B: ");
-int.TryParse(Console.ReadLine()!, out xb);
-Console.Write($"Введите координату Y точки B: ");
-int.TryParse(Console.ReadLine()!, out yb);
-Console.Write($"Введите координату Z точки B: ");
-int.TryParse(Console.ReadLine()!, out zb);
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
 
-double res = Math.Sqrt((xa-xb)*(xa-xb)+(ya-yb)*(ya-yb)+(za-zb)*(za-zb));
-Console.Write($"Расстояние между точками A и B = {res}");
+int i;
+for (i = 1; i <= N; i++)
+     Console.Write($"{i * i * i} ");
 
