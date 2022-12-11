824b7fcb95d0f8856dbe3651ba068b8faf2814a9
Sasha <a.magerova@list.ru>
2022-12-11 16:30:01 +0300

    task25

---
+++ Lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача25
+
+int A, B;
+Console.Write($"Введите число A: ");
+int.TryParse(Console.ReadLine()!, out A);
+Console.Write($"Введите число B: ");
+int.TryParse(Console.ReadLine()!, out B);
+
+if (B == 0)
+    Console.Write(1);
+
+else
+{
+    int res = 1;
+    for (int i = 0; i < B; i++)
+         res *= A;
+         Console.Write(res);
+}
+
