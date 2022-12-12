9307ce7e7272fcf15fbc90b25aa78b07f7041b95
Sasha <a.magerova@list.ru>
2022-12-13 00:36:51 +0300

    task34

---
+++ Lesson5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача34
+
+int[] array = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+     array [i] = new Random().Next(100,1000);     
+Print(array);
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+       sum +=1; 
+    }
+}
+Console.Write($"--> {sum} ");
+
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+         Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
