d57d6d8cfdbdef6a32cbad9f725da1e7c9733291
Sasha <a.magerova@list.ru>
2022-12-14 02:02:40 +0300

    task41

---
+++ Lesson6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача41
+
+string[] numbers = Console.ReadLine()!.Split(' ');
+int[] array = new int[numbers.Length];
+
+for(int i = 0; i < numbers.Length; i++)
+    {
+        int.TryParse(numbers[i], out array[i]);
+        Console.Write($"{array[i]} ");
+    }
+
+int n = 0;
+foreach(var i in array)
+    if (i > 0)
+        n++;
+
+Console.Write($"\n{n} чисел больше 0");
+
