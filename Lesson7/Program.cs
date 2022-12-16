5daebb6ceb7f28f05a0ed3f5b460a9172fab3682
Sasha <a.magerova@list.ru>
2022-12-16 16:50:09 +0300

    task50

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -1,13 +1,24 @@
-﻿// Задача47
+﻿// Задача50
 
-double[,] array = new double[6,7];
+int[,] array = new int[3,3];
+int n = 9;
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(0); j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = Math.Round(new Random().NextDouble() * 19 - 9.9, 2);
-        Console.Write($"{array[i, j]}" );
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} " );
     }
     Console.WriteLine();
 }
+
+bool find = false;
+
+foreach(var i in array)
+    if (i == n) {find = true; break; }  
+
+if (find)
+    Console.Write($"Число присутствует в массиве");
+else
+    Console.Write($"Такого числа нет");
