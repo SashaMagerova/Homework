9f9015fbf220e9dbba661c1c345f035d0d43a977
Sasha <a.magerova@list.ru>
2022-12-16 16:32:10 +0300

    task47

---
+++ Lesson7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача47
+
+double[,] array = new double[6,7];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        array[i, j] = Math.Round(new Random().NextDouble() * 19 - 9.9, 2);
+        Console.Write($"{array[i, j]}" );
+    }
+    Console.WriteLine();
+}
