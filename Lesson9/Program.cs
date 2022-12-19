f8a8fdcb8e6c3889a865455cbdf390236ee396e1
Sasha <a.magerova@list.ru>
2022-12-19 17:16:54 +0300

    task64

---
+++ Lesson9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача64
+int n = 10;
+
+Console.Write($"{PrintNums(n, 1)}");
+
+string PrintNums(int left, int right)
+{
+    if (left <= right)
+       return left.ToString();
+    return $"{left} {PrintNums(left - 1, right)}";  
+}
